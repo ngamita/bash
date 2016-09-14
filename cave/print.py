27027895e7e6b14839b424f57bd535ea03939018
ngamita <ngamita@gmail.com>
2016-09-15 00:02:25 +0300

    print tests

---
+++ cave/print.py
@@ -0,0 +1,12 @@
+# Richard Ngamita "ngamita@gmail.com"
+# Basic print job. 
+print "Hello Uncle print, i'm very happy to be here"
+print "Hello Uncle pring, \ni'm very happy to be here"
+print "I\'m so happy to be in uncle\'s cave"
+print "%s %s %s" %('I', 'am', 'concatenating strings')
+print '%s %d' %('remember everything starts with a', 0)
+print '%s %f %s' %('Value of PI i.e.', 3.14, 'just another scientific opinion')
+print '%s %4d' %('The number with length 4', 25)
+print '\n%s %4.2f' %('Float with length 4 printed to 2 decimals places', 2.5051995)
+print '%d' %(-122)
+print '%+d' %(19)
