1a908745e5c677def32ba71ede160fc38a496eb8
ngamita <ngamita@gmail.com>
2016-09-20 00:46:35 +0300

    Fixed while loop

--- cave/loops.py
+++ cave/loops.py
@@ -1,3 +1,4 @@
+#! usr/bin/python
 """
 Given a color as input you'll have to output the personality of the person as follows.
 
@@ -10,23 +11,34 @@ Given a color as input you'll have to output the personality of the person as fo
 		RED     ->  loving 
 """
 
-# print("please enter color:")
-color = raw_input('Please enter color: ').lower()
-#color = "RED"
+color = ""
+while(color != "exit"):
+    # print("please enter color:")
+    
+    #color = "RED"
 
-if(color == "violet"):
-    print("optimistic")
-elif (color == "indigo"):
-    print "independent"
-elif(color == "blue"):
-    print("practical")
-elif(color == "green"):
-    print("conservative")
-elif(color == "yellow"):
-    print("trustworthy")
-elif(color == "orange"):
-    print("reliable")
-elif(color == "red"):
-    print("loving")
-else:
-    print("Sorry, we didn't understand that color!")
+    if(color == "violet"):
+        print("optimistic")
+    elif (color == "indigo"):
+        print "independent"
+    elif(color == "blue"):
+        print("practical")
+    elif(color == "green"):
+        print("conservative")
+    elif(color == "yellow"):
+        print("trustworthy")
+    elif(color == "orange"):
+        print("reliable")
+    elif(color == "red"):
+        print("loving")
+    else:
+        print("Sorry, we didn't understand that color!")
+    color = raw_input('Please enter color: ').lower()
+
+"""
+names = "ngamita"
+len(names)
+i = 0
+while(i < len(names)):
+    print names[i]
+    i+=1"""
