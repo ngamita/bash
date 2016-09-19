6998ccf193591b448c8ec06be80c7950c2795602
ngamita <ngamita@gmail.com>
2016-09-19 23:40:34 +0300

    Loops refresher

---
+++ cave/loops.py
@@ -0,0 +1,30 @@
+"""
+Given a color as input you'll have to output the personality of the person as follows.
+
+		VIOLET	->	optimistic
+		INDIGO	->  independent
+		BLUE    ->  practical
+		GREEN   ->  conservative
+		YELLOW  ->  trustworthy 
+		ORANGE  ->  reliable 
+		RED     ->  loving 
+"""
+
+# print("please enter color:")
+
+color = "violet"
+
+if(color == "violet"):
+    print("optimistic")
+elif (color == "indigo"):
+    print "independent"
+elif(color == "blue"):
+    print("practical")
+elif(color == "green"):
+    print("conservative")
+elif(color == "yellow"):
+    print("trustworthy")
+elif(color == "orange"):
+    print("reliable
+elif(colot == "red"):
+    print("loving")
