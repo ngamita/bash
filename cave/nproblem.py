cbafab1b0e5f9c5f289104e77a694d1bea8b9e02
ngamita <ngamita@gmail.com>
2016-09-20 23:49:20 +0300

    Fixes to while and for loops

--- cave/nproblem.py
+++ cave/nproblem.py
@@ -7,4 +7,10 @@ Created on Tue Sep 20 22:04:14 2016
 
 @author: ngamita
 """
-
+n = 0
+while(n <= 100):
+    for num in range(0,n):
+        print(num**3)
+    n = int(raw_input('Enter n: '))
+        #n+=1
+        #print(n**
