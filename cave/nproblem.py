5105406f06ebece132d5a94a413b8ddcf84c4798
ngamita <ngamita@gmail.com>
2016-09-20 22:07:19 +0300

    Adding nproblems solution

---
+++ cave/nproblem.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Problem: Given input N, print cubes of first N natural numbers.
+Also make sure that N is less than or equal to 100. 
+
+Created on Tue Sep 20 22:04:14 2016
+
+@author: ngamita
+"""
+
